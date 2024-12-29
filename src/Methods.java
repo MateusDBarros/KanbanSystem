a7079e0a3b92ecbfc343e04cf6799c455b08be21
Mateus de Barros <97922225+MateusDBarros@users.noreply.github.com>
2024-12-29 17:30:27 -0300

    Update Methods.java

--- src/Methods.java
+++ src/Methods.java
@@ -3,11 +3,22 @@ import java.util.ArrayList;
 public class Methods{
 
 
+    // Sequências ANSI
+    static final String RESET = "\u001B[0m";
+    static final String BLUE = "\u001B[34m";
+
     public static void showAll(ArrayList<Pessoa> pessoas) {
-        System.out.printf("%-8s | %-25s | %-15s\n", "Registro", "Nome", "Status");
-        System.out.println("----------------------------------------------");
-        for (Pessoa pessoa : pessoas)
-            System.out.printf("%-8s | %-25s | %-15s\n", pessoa.getId(), pessoa.getName(), pessoa.getLevel().getDescription());
+        if (pessoas == null || pessoas.isEmpty()) {
+            System.out.println("Nenhum registro encontrado");
+            return;
+        }
+        System.out.println(BLUE + "+----------+-------------------------+----------------+" + RESET);
+        System.out.println(BLUE + "| Registro | Nome                    | Status         |" + RESET);
+        System.out.println(BLUE + "+----------+-------------------------+----------------+" + RESET);
+        for (Pessoa pessoa : pessoas) {
+            System.out.printf("| %-8s | %-23s | %-14s |\n", pessoa.getId(), pessoa.getName(), pessoa.getLevel().getDescription());
+        }
+        System.out.println(BLUE + "+----------+-------------------------+----------------+" + RESET);
     }
 
     public static int search(ArrayList<Pessoa> pessoas, String name, int l, int r) {
@@ -29,10 +40,10 @@ public class Methods{
             return search(pessoas, name, mid + 1, r);
     }
 
-    public static int confirm(char x) {
-        if (x == 'y'){
+    public static int confirm(String input) {
+        if (input.equalsIgnoreCase("y")){
             return 1;
-        } else if (x == 'n') {
+        } else if (input.equalsIgnoreCase("n")) {
             return 0;
         }
         else return -1;
